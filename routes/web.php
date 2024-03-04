099b4ac9bb11b5fd272fceba959fec68579786d2
NugrohoRahmanto <nugrohorahmanto24@gmail.com>
2024-03-04 18:25:46 +0700

    [STYLE] Add new page & source, change readme

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,9 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+Route::get('/about', function () {
+    return view('about');
 });

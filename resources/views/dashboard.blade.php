88a91dcaccf57e898210a2dad91352622da1106a
Anandito Satria Asyraf <ananditosatriaa181@gmail.com>
2024-03-17 22:26:12 +0700

    [Adjustment] Adjs Color Layout Admin Panel

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,23 @@
+@extends('layout')
+@section('content')
+
+@if(Session::has('userInfo'))
+    @php
+        $data = Session::get('userInfo');
+    @endphp
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                <h1 class="h3 mb-4 text-gray-800 bold-text">Selamat Datang di Tel-U Cafetaria</h1>
+                <div class="card mb-4 py-3 border-bottom-warning">
+                    <div class="card-body black-text">
+                        <p>{{ $data->getNickname() }}</p>
+                        <p>{{ $data->getfullName() }}</p>
+                        <p>{{ $data->getRole() }}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endif
+@endsection
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -11,14 +11,14 @@
     <meta name="author" content="">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>KANTIN TULT</title>
+    <title>Tel-U Cafetaria</title>
 
     <link href="{{ asset('fontawesome/css/all.min.css') }}" rel="stylesheet" type="text/css">
     <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
 
-    <link href="{{ asset('css/sb-admin-2.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/sb-admin-2.css') }}" rel="stylesheet">
     <link href="{{ asset('css/support-index.css') }}" rel="stylesheet">
 
     <link href="{{ asset('vendor/datatables/dataTables.bootstrap4.min.css') }}" rel="stylesheet">
@@ -37,7 +37,7 @@
                 <div class="sidebar-brand-icon rotate-n-15">
                     <i class="fa-solid fa-utensils"></i>
                 </div>
-                <div class="sidebar-brand-text mx-3">KANTIN TULT</div>
+                <div class="sidebar-brand-text mx-3">Tel-U Cafetaria</div>
             </a>
 
             <hr class="sidebar-divider my-0">
@@ -203,7 +203,7 @@
                     <div class="container my-auto">
                         <div class="copyright text-center my-auto black-text bold">
                             {{-- <span>Copyright &copy; KANTIN TULT 2024</span> --}}
-                            <span>KANTIN TULT 2024</span>
+                            <span>TEL-U CAFETARIA 2024</span>
                         </div>
                     </div>
                 </footer>
